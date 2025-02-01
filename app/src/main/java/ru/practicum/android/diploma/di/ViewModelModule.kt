0dafaabb7e887f8f7ee7c2c6ff81799583bf37d4
DanilaLort <105969240+DanilaLort@users.noreply.github.com>
2025-02-01 21:11:34 +0300

    Update ViewModelModule.kt

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -1,11 +1,11 @@
 package ru.practicum.android.diploma.di
 
-import org.koin.dsl.module
 import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
 import ru.practicum.android.diploma.domain.models.FilterParameters
-import ru.practicum.android.diploma.presentation.ui.vacancy.VacancyViewModel
 import ru.practicum.android.diploma.presentation.ui.favourites.FavouritesViewModel
 import ru.practicum.android.diploma.presentation.ui.search.SearchViewModel
+import ru.practicum.android.diploma.presentation.ui.vacancy.VacancyViewModel
 
 val viewModelModule = module {
 
