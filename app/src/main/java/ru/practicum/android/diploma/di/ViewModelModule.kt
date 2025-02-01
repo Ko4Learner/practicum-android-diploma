1d54347cda2865bfc6768c0320a03a48a58e7020
Kvadrokom <lortrem@mail.ru>
2025-02-01 21:02:23 +0300

    code fix

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -1,8 +1,7 @@
 package ru.practicum.android.diploma.di
 
-
-import org.koin.core.module.dsl.*
 import org.koin.dsl.module
+import org.koin.core.module.dsl.viewModel
 import ru.practicum.android.diploma.domain.models.FilterParameters
 import ru.practicum.android.diploma.presentation.ui.vacancy.VacancyViewModel
 import ru.practicum.android.diploma.presentation.ui.favourites.FavouritesViewModel
