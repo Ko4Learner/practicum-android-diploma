829e98dbecb21e0bb267c72da78e7c73b4478fbc
Ko4Learner <teterev.vakuum@mail.ru>
2025-02-04 00:13:23 +0300

    fix code

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -6,8 +6,10 @@ import ru.practicum.android.diploma.domain.models.FilterParameters
 import ru.practicum.android.diploma.presentation.ui.favourites.FavouritesViewModel
 import ru.practicum.android.diploma.presentation.ui.search.SearchViewModel
 import ru.practicum.android.diploma.presentation.ui.vacancy.VacancyViewModel
+import ru.practicum.android.diploma.presentation.ui.filter.settings.FilterSettingsViewModel
 
 val viewModelModule = module {
+    viewModelOf(::FilterSettingsViewModel)
     viewModelOf(::SearchViewModel)
     viewModelOf(::VacancyViewModel)
     viewModelOf(::FavouritesViewModel)
