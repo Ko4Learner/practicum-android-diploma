94e7c564e4dadbcdc7fc1354937c300d9c218aec
Ko4Learner <teterev.vakuum@mail.ru>
2025-02-05 00:14:29 +0300

    fix code_2

--- app/src/main/java/ru/practicum/android/diploma/domain/models/FilterParameters.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/FilterParameters.kt
@@ -1,6 +1,6 @@
 package ru.practicum.android.diploma.domain.models
 
-data class FilterParameters(
+class FilterParameters(
     var country: Country? = null,
     var area: Area? = null,
     var industry: Industry? = null,
@@ -25,9 +25,42 @@ data class FilterParameters(
         } else {
             if (country != null) result[AREA_QUERY] = country!!.id
         }
-        if (expectedSalary != null && expectedSalary != 0) result[SALARY_QUERY] = expectedSalary.toString()
+        if (expectedSalary != null && expectedSalary != 0) result[SALARY_QUERY] =
+            expectedSalary.toString()
         if (onlyWithSalary != null) result[ONLY_WITH_SALARY_QUERY] = onlyWithSalary.toString()
         if (industry != null) result[INDUSTRY] = industry!!.id
         return result
+    }
+
+    fun copy(
+        country: Country? = this.country,
+        area: Area? = this.area,
+        industry: Industry? = this.industry,
+        expectedSalary: Int? = this.expectedSalary,
+        onlyWithSalary: Boolean? = this.onlyWithSalary
+    ) = FilterParameters(country, area, industry, expectedSalary, onlyWithSalary)
+
+    override fun hashCode(): Int {
+        var result = country?.hashCode() ?: 0
+        result = 31 * result + (area?.hashCode() ?: 0)
+        result = 31 * result + (industry?.hashCode() ?: 0)
+        result = 31 * result + (expectedSalary ?: 0)
+        result = 31 * result + (onlyWithSalary?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FilterParameters
+
+        if (country != other.country) return false
+        if (area != other.area) return false
+        if (industry != other.industry) return false
+        if (expectedSalary != other.expectedSalary) return false
+        if (onlyWithSalary != other.onlyWithSalary) return false
+
+        return true
     }
 }
