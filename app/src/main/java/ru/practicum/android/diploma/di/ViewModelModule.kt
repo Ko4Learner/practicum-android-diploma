9e2772eba46166ea3a3aca5b0aae1ec88f809427
Ko4Learner <teterev.vakuum@mail.ru>
2025-02-04 14:38:04 +0300

    viewModel module (viewModelOf) fix

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -1,31 +1,25 @@
 package ru.practicum.android.diploma.di
 
-import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import ru.practicum.android.diploma.domain.models.FilterParameters
 import ru.practicum.android.diploma.presentation.ui.favourites.FavouritesViewModel
-import ru.practicum.android.diploma.presentation.ui.filter.industry.SelectIndustryViewModel
 import ru.practicum.android.diploma.presentation.ui.search.SearchViewModel
 import ru.practicum.android.diploma.presentation.ui.vacancy.VacancyViewModel
+import ru.practicum.android.diploma.presentation.ui.filter.settings.FilterSettingsViewModel
+import ru.practicum.android.diploma.presentation.ui.filter.industry.SelectIndustryViewModel
+import ru.practicum.android.diploma.presentation.ui.filter.workplace.SelectWorkplaceViewModel
+import ru.practicum.android.diploma.presentation.ui.filter.workplace.country.SelectCountryViewModel
+import ru.practicum.android.diploma.presentation.ui.filter.workplace.region.SelectRegionViewModel
 
 val viewModelModule = module {
-
-    viewModel {
-        SearchViewModel(get())
-    }
-
-    viewModel {
-        VacancyViewModel(get())
-    }
-
-    viewModel {
-        FavouritesViewModel(get())
-    }
-
-    viewModel {
-        SelectIndustryViewModel(get(), get())
-    }
-
+    viewModelOf(::FilterSettingsViewModel)
+    viewModelOf(::SearchViewModel)
+    viewModelOf(::VacancyViewModel)
+    viewModelOf(::FavouritesViewModel)
+    viewModelOf(::SelectIndustryViewModel)
+    viewModelOf(::SelectWorkplaceViewModel)
+    viewModelOf(::SelectCountryViewModel)
+    viewModelOf(::SelectRegionViewModel)
     single { FilterParameters() }
-
 }
